e2415b81970d589777dd56eef0d7d70dda9c5c3f
Julian Buchholz <github@julianbuchholz.com>
2022-09-18 12:40:59 +0200

    add: margin for p

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,41 +10,51 @@ export default function Index() {
           metal balls at a tinier wooden ball. It is great fun, trust us.
         </p>
         <h2>This is how you play</h2>
-        Bocce is traditionally played on a natural soil or asphalt court up to
-        27.5 metres (90 ft) in length and 2.5 to 4 metres (8.2 to 13.1 ft)
-        wide.[14] While the court walls are traditionally made of wood or stone,
-        many social leagues and Special Olympics programs now use inflatable
-        'Packabocce' PVC courts due to their portability and ease of
-        storage.[15][16] Bocce balls can be made of wood (traditional),
-        metal,[14] baked clay, or various kinds of plastic. Unlike lawn bowls,
-        bocce balls are spherical and have no inbuilt bias. A game can be
-        conducted between two players, or two teams of two, three, or four. A
-        match is started by a randomly chosen side being given the opportunity
-        to throw a smaller ball, the jack (called a boccino ('little bocce') or
-        pallino ('bullet' or 'little ball') in Italian, depending on local
-        custom), from one end of the court into a zone 5 metres (16 ft) in
-        length, ending 2.5 metres (8.2 ft) from the far end of the court. If the
-        first team misses twice, the other team is awarded the opportunity to
-        place the jack anywhere they choose within the prescribed zone.[6]
-        Casual play is common in reasonably flat areas of parks and yards
-        lacking a Bocce court, but players should agree to the minimum and
-        maximum distance the jack may be thrown before play begins. The side
-        that first attempted to place the jack is given the opportunity to bowl
-        first. Once the first bowl has taken place, the other side has the
-        opportunity to bowl. From then on, the side which does not have the ball
-        closest to the jack has a chance to bowl, up until one side or the other
-        has used their four balls. At that point, the other side bowls its
-        remaining balls. The object of the game is for a team to get as many of
-        its balls as possible closer to the target ball (jack, boccino, pallino)
-        than the opposing team. The team with the closest ball to the jack is
-        the only team that can score points in any frame. The scoring team
-        receives one point for each of their balls that is closer to the jack
-        than the closest ball of the other team. The length of a game varies by
-        region but is typically from 7 to 13 points.[17][6] Players are
-        permitted to throw the ball in the air using an underarm action. This is
-        generally used to knock either the jack or another ball away to attain a
-        more favorable position. Tactics can get quite complex when players have
-        sufficient control over the ball to throw or roll it accurately.
+        <p>
+          Bocce is traditionally played on a natural soil or asphalt court up to
+          27.5 metres in length and 2.5 to 4 metres wide. While the court walls
+          are traditionally made of wood or stone, many social leagues and
+          Special Olympics programs now use inflatable 'Packabocce' PVC courts
+          due to their portability and ease of storage. Bocce balls can be made
+          of wood (traditional), metal, baked clay, or various kinds of plastic.
+          Unlike lawn bowls, bocce balls are spherical and have no inbuilt bias.
+        </p>
+        <p>
+          A game can be conducted between two players, or two teams of two,
+          three, or four. A match is started by a randomly chosen side being
+          given the opportunity to throw a smaller ball, the jack (called a
+          boccino ('little bocce') or pallino ('bullet' or 'little ball') in
+          Italian, depending on local custom), from one end of the court into a
+          zone 5 metres in length, ending 2.5 metres from the far end of the
+          court. If the first team misses twice, the other team is awarded the
+          opportunity to place the jack anywhere they choose within the
+          prescribed zone. Casual play is common in reasonably flat areas of
+          parks and yards lacking a Bocce court, but players should agree to the
+          minimum and maximum distance the jack may be thrown before play
+          begins.
+        </p>
+        <p>
+          The side that first attempted to place the jack is given the
+          opportunity to bowl first. Once the first bowl has taken place, the
+          other side has the opportunity to bowl. From then on, the side which
+          does not have the ball closest to the jack has a chance to bowl, up
+          until one side or the other has used their four balls. At that point,
+          the other side bowls its remaining balls. The object of the game is
+          for a team to get as many of its balls as possible closer to the
+          target ball (jack, boccino, pallino) than the opposing team. The team
+          with the closest ball to the jack is the only team that can score
+          points in any frame. The scoring team receives one point for each of
+          their balls that is closer to the jack than the closest ball of the
+          other team. The length of a game varies by region but is typically
+          from 7 to 13 points.
+        </p>
+        <p>
+          Players are permitted to throw the ball in the air using an underarm
+          action. This is generally used to knock either the jack or another
+          ball away to attain a more favorable position. Tactics can get quite
+          complex when players have sufficient control over the ball to throw or
+          roll it accurately.
+        </p>
       </>
     </Layout>
   )
